394905e40b72a0656513e868ab49c0887c0d313e
Lucas Gomes <lucasggomees@gmail.com>
2020-04-25 12:04:27 +0100

    Added a resposinve navbar

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,16 @@
-console.log("hello");
+"use strict";
+
+// Responsive nav bar
+(function () {
+	const navLinks = document.querySelector(".navbar-links");
+	const navbar = document.querySelector("nav");
+	const brand = document.querySelector(".brand-name");
+
+	// Closes navbar after choosing a section link
+	navbar.addEventListener("click", (event) => {
+		if (event.target !== navbar && event.target !== brand) {
+			// console.log(event.target);
+			navLinks.classList.toggle("active");
+		}
+	});
+})();
