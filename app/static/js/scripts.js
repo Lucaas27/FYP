812fa04f635569183cebeff41b0522bb848fdc71
Lucas Gomes <lucasggomees@gmail.com>
2020-04-28 01:49:53 +0100

    Jquery animation to close alerts

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -14,3 +14,14 @@
 		}
 	});
 })();
+
+// Close alerts
+$(document).ready(function () {
+	window.setTimeout(function () {
+		$(".alert")
+			.fadeTo(1000, 0)
+			.slideUp(500, function () {
+				$(this).remove();
+			});
+	}, 2500);
+});
